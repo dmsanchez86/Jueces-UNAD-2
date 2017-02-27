e8aa34f627718e6f4026b086e93db3b39aa4793f
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2017-02-27 14:22:33 -0500

    se creo funcion que me carga el pdf del inicio

--- src/js/controllers/homeCtrl.js
+++ src/js/controllers/homeCtrl.js
@@ -18,9 +18,29 @@ angular
   .module('judgesApp')
   .controller(
 		'homeCtrl',
-		function($scope){
+		function($scope, API, $sce){
 			// variable que controlara cuando mostrar el menu
 			$scope.$parent.acceptTerms = true;
 			$scope.$parent.background = 'bg-home';
+
+            // variable que guarda la ruta del pdf
+            $scope.dataPDF = {
+                ruta: null
+            };
+
+            // consultamos el sevicio que me trae el visualizar pdf del home
+            API.call(
+                'dicente/pdfinicio',
+                'GET',
+                {},
+                function(response){
+                    if(response.status == "success"){
+                        $scope.dataPDF.ruta = response.data.ruta;
+                    }else{
+                        console.log('No se encontro pdf a mostrar');
+                    }
+
+                    $scope.dataPDF.ruta = $sce.trustAsResourceUrl( $scope.dataPDF.ruta );
+                });
 		}
 	);
